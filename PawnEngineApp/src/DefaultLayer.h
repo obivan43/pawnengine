b269e317a6e0b59d35abaa7676dddee64396f3d2
obi_van <obivan43@gmail.com>
2021-02-06 02:04:29 +0200

    Small refactoring

--- PawnEngineApp/src/DefaultLayer.h
+++ PawnEngineApp/src/DefaultLayer.h
@@ -7,6 +7,13 @@ namespace pawn {
 	class DefaultLayer : public Layer {
 		
 		public:
+			DefaultLayer() = default;
+			DefaultLayer(const DefaultLayer& other) = delete;
+			DefaultLayer(DefaultLayer&& other) noexcept = default;
+
+			DefaultLayer& operator=(const DefaultLayer& other) = delete;
+			DefaultLayer& operator=(DefaultLayer&& other) noexcept = delete;
+		
 			virtual void OnInit() override;
 			virtual void OnUpdate(Clock clock) override;
 			virtual void OnRelease() override;
--- PawnEngineApp/src/Layer.h
+++ PawnEngineApp/src/Layer.h
@@ -5,6 +5,15 @@ namespace pawn {
 	class Layer : public Observer {
 		
 		public:
+			Layer() = default;
+			Layer(const Layer& other) = delete;
+			Layer(Layer&& other) noexcept = default;
+		
+			Layer& operator=(const Layer& other) = delete;
+			Layer& operator=(Layer&& other) noexcept = delete;
+
+			virtual ~Layer() = default;
+		
 			virtual void OnInit();
 			virtual void OnUpdate(Clock clock);
 			virtual void OnRelease();
