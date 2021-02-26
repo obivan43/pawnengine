4996fbdd0dc3b70b7f34dfd760987f2317b757a0
obi_van <obivan43@gmail.com>
2021-02-26 18:17:03 +0200

    Improved transformation matrix code

--- Math/pch/pch.h
+++ Math/pch/pch.h
@@ -1,3 +1,4 @@
 ﻿#pragma once
 
 #include "glm.hpp"
+#include "gtx/quaternion.hpp"
--- Math/src/Transformation.h
+++ Math/src/Transformation.h
@@ -1,11 +1,33 @@
 ﻿#pragma once
 
+#include "glm.hpp"
+
 namespace pawn {
 	
-	struct Transformation {
-		struct {
-			float transformation[4][4];
-		} transformation;
+	class Transformation {
+
+		public:
+
+			Transformation(
+				const glm::vec3& position = glm::vec3(),
+				const glm::vec3& rotation = glm::vec3(),
+				const glm::vec3& scale = glm::vec3(1.0f, 1.0f, 1.0f)
+			);
+
+			const glm::vec3& GetPosition() const { return m_Position; }
+			const glm::vec3& GetRotation() const { return m_Rotation; }
+			const glm::vec3& GetScale() const { return m_Scale; }
+
+			void SetPosition(const glm::vec3& position);
+			void SetRotation(const glm::vec3& rotation);
+			void SetScale(const glm::vec3& scale);
+
+			glm::mat4 GetModelMatrix();
+
+		private:
+			glm::vec3 m_Position;
+			glm::vec3 m_Rotation;
+			glm::vec3 m_Scale;
 	};
 	
 }
