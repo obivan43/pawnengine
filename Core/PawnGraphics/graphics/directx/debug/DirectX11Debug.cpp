d37b11a6eca25e5f2afb88ce265449d00f3c492e
obi_van <obivan43@gmail.com>
2021-07-20 18:30:57 +0300

    Added debug graphics

---
+++ Core/PawnGraphics/graphics/directx/debug/DirectX11Debug.cpp
@@ -0,0 +1,16 @@
+#include "DirectX11Debug.h"
+
+#include "PawnUtils/utils/logger/Logger.h"
+
+#if defined(DEBUG) | defined(_DEBUG)
+#ifdef DirectX11Call
+
+void DirectX11CallDebug(HRESULT result, const char* file, unsigned int line, const char* expression) {
+	if (FAILED(result))  {
+		CONSOLE_ERROR("DirectX call error: {}, {}, {}", file, line, expression)
+		__debugbreak();
+	}
+}
+
+#endif
+#endif
