08472414d0cc08cc2e75554eafa0f6d8771a1a0f
obi_van <obivan43@gmail.com>
2021-10-10 18:51:41 +0300

    Added simple rotation script

---
+++ Editor/res/assets/scripts/Rotation.lua
@@ -0,0 +1,13 @@
+function create()
+end
+
+function update(dt)
+	local angle = 1.0 * dt
+	local entity = GameEntity.new(current_entity())
+	local entityTransform = entity.transformation
+
+	entityTransform.rotation.x = entityTransform.rotation.x + angle
+	entityTransform.rotation.y = entityTransform.rotation.y + angle * 5.0
+	entityTransform.rotation.z = entityTransform.rotation.z + angle
+	entity.transformation = entityTransform
+end
